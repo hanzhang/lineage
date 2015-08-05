14291133821ff3cfd9bcd01bf41f26bc2a49f704
abzgupta <gSchool@Datas-Mac-mini-15.local>
2015-08-05 11:03:47 -0700

    Updates to scrape files

--- create_sql_tables.py
+++ create_sql_tables.py
@@ -40,4 +40,38 @@ def create_song_table(cur, con):
         pass
 
     # Example entry:
-    #cur.execute('INSERT INTO songs VALUES(%s, %s, %s, %s, %s, %s)', ('/Lil-Jon/Lovers-%26-Friends/', 'Lovers & Friends', 'Crunk Juice', 'TVT', 2004, '/genre/Hip-Hop/'))
+    #cur.execute('INSERT INTO songs VALUES(%s, %s, %s, %s, %d, %s)', ('/Lil-Jon/Lovers-%26-Friends/', 'Lovers & Friends', 'Crunk Juice', 'TVT', 2004, '/genre/Hip-Hop/'))
+
+# Create artist to song mapping table in SQL
+def create_artist_to_song_table(cur, con):
+    try:
+        cur.execute('CREATE TABLE artist_to_song (artist_url varchar, \
+                                                  song_url varchar)')
+    except:
+        pass
+
+    # Example entry:
+    #cur.execute('INSERT INTO artists VALUES(%s, %s)', ('/Kanye-West/', '/Kanye-West/Mercy/'))
+
+# Create producer to song mapping table in SQL
+def create_producer_to_song_table(cur, con):
+    try:
+        cur.execute('CREATE TABLE producer_to_song (producer_url varchar, \
+                                                    song_url varchar)')
+    except:
+        pass
+
+    # Exmaple entry:
+    #cur.execute('INSERT INTO producers VALUES(%s, %s)', ('/Kanye-West/', '/Kanye-West/Mercy/Power'))
+
+# Create song to song relatioship table in SQL
+def create_song_to_song_table(cur, con):
+    try:
+        cur.execute('CREATE TABLE song_to_song (song1_url varchar, \
+                                                song2_url varchar, \
+                                                relationship integer)')
+    except:
+        pass
+
+    # Example entry:
+    #cur.execute('INSERT INTO songs VALUES(%s, %s, %d)', ('/Black-Eyed-Peas/Boom-Boom-Pow/', '/Daft-Punk/Harder,-Better,-Faster,-Stronger/', 1))
